1919e13d0e18b9ed87024c618750c2b5653e1701
tabir123 <86651458+tabir123@users.noreply.github.com>
2021-07-30 20:59:41 +0700

    Delete SP2-LATIHAN2 directory

--- SP2-LATIHAN2/tugasarray2.js
+++
@@ -1,4 +0,0 @@
-var reactnative = ['hafif','dayat','wandi']
-var reactjs = ['yusuf','luthfi','tompel']
-
-document.write('java script division : ' + reactnative + ',' + reactjs)
--- SP2-LATIHAN2/tugasarray3.js
+++
@@ -1,5 +0,0 @@
-var santriPondok = [ '', 'wandi', 'aldi', 'acep', 'putra', 'hafif', 'syofyan', 'rahmad', ]
-
-for(var i = 1; i < santriPondok.length; i++){
-    console.log(i + '.' + '' + santriPondok[i])
-}
